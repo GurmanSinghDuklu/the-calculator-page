5934438b148ecb1ea1e24296b8fb88ae82082820
Gurman Singh Duklu <gurmanduklu@gmail.com>
2026-03-09 19:26:01 +0000

    Revert "Add global navbar to all pages"
    This reverts commit 24f4157820c8845d9b4794f2010f6d5ce193c3af.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { SEO } from "@/components/SEO";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ScrollToTop from "@/components/ScrollToTop";
-import { Navbar } from "@/components/Navbar";
 
 const App = () => {
   return (
@@ -14,8 +13,6 @@ const App = () => {
           title="The Calculator Page - Professional Financial Tools"
           description="Expert financial calculators, mortgage advice tools, and unit converters."
         />
-
-        <Navbar />
 
         <main className="flex-grow">
           <Outlet />
